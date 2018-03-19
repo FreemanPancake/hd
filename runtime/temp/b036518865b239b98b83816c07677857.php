fe55497f8996bf00226dce6fd4db68ff1a526bac
young <lightwish@126.com>
2018-03-19 18:20:33 +0800

    add css

--- runtime/temp/b036518865b239b98b83816c07677857.php
+++ runtime/temp/b036518865b239b98b83816c07677857.php
@@ -1,3 +1,4 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:2:{s:66:"/var/www/html/hd/public/../application/index/view/index/index.html";i:1521453557;s:49:"/var/www/html/hd/application/index/view/base.html";i:1521425466;}*/ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,8 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,Chrome=1"/>
     <meta name="renderer" content="webkit"/>
     <title>Join Now</title>
-    <link rel="stylesheet" type="text/css" href="__STATIC__/static/css/bootstrap.css"/>
-    <link rel="stylesheet" type="text/css" href="__STATIC__/static/css/main.css"/>
+    <link rel="stylesheet" type="text/css" href="/hd/public/static/css/bootstrap.css"/>
+    <link rel="stylesheet" type="text/css" href="/hd/public/static/css/main.css"/>
 </head>
 <body>
 <div class="container">
@@ -48,10 +49,44 @@
         </nav>
     </header>
     </div>
-    {block name='content'}
-    {/block}
+    
+<div class="row">
+    <div class="col-lg-12">
+        <div class="head">
+            <span>热门活动</span>
+        </div>
+        <div class="ac-list">
+            <hr>
+            <div class="act-item">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="act-header">
+                            <span class="act-cat" style="text-align-all: center">精选优惠</span>
+                            <span class="act-name">魅族应用商店抽5元话费活动 限魅族手机参与！</span>
+                        </div>
+                    </div>
+                    <div class="col-md-3 act-meta-frame" style="border-left: 1px dotted lightgray">
+                        <div class="act-meta">
+                            <span class="glyphicon glyphicon-user" style="font-size:18px;">&nbsp;</span>
+                            <span>一轮明月照九州</span>
+                        </div>
+                        <div class="act-meta">
+                                <span class="glyphicon glyphicon-lock">&nbsp;</span>
+                                <span>中国银行62开头信用卡用户可参与</span>
+                        </div>
+                        <div class="act-meta">
+                            <span class="glyphicon glyphicon-map-marker">&nbsp;</span>
+                            <span>万达广场</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-<script src="__STATIC__/static/js/jquery.min.js" type="text/javascript"></script>
-<script src="__STATIC__/static/js/bootstrap.min.js" type="text/javascript"></script>
+
+</div>
+<script src="/hd/public/static/js/jquery.min.js" type="text/javascript"></script>
+<script src="/hd/public/static/js/bootstrap.min.js" type="text/javascript"></script>
 </body>
 </html>
